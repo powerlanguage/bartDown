4c7109e0db430c42415d8bf44a858ba354ca6d7d
powerlanguage <josh.wardle@gmail.com>
2017-12-13 22:41:42 -0800

    Add min time cutoff and todos

--- bartDown.js
+++ bartDown.js
@@ -1,6 +1,17 @@
+
+
 // Number of departures we want to display for each direction
 const limit = 3;
-const station = "19th"
+const station = "woak"
+const minute_cutoff = 3;
+
+// TODO
+// Get station and other things from URL params
+// URLSearchParams()
+
+// TODO:
+// Have font size be related to number of stations
+// displayed
 
 $.get(`http://api.bart.gov/api/etd.aspx?key=${API_KEY}&cmd=etd&orig=${station}&json=y`, bartDown);
 
@@ -20,6 +31,14 @@ function bartDown(data) {
   var estimates = data.root.station[0].etd.reduce((acc, etd) => {
     return acc.concat(etd.estimate);
   }, []);
+
+  // TODO:
+  // filter out any destinations we aren't interested in.
+  // Distination is a parent of estimates, so needs to be done
+  // above
+
+  // Filter estimates that don't match criteria
+  estimates = estimates.filter(estimate => estimate.minutes > minute_cutoff);
 
   // Transform 'Leaving' to 00 and ensure all times are double digits
   estimates.forEach(estimate => {
